c4c4bb54e6fe9486b19cdaaf99a4e9942ba1b015
Oluwatimilehin Adeniran <timitom60@gmail.com>
2022-12-26 21:23:38 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+"use script";
+
+// Grab the toggle button and elements with class "dark"
+const toggleBtn = document.querySelector(".toggle_fill");
+const labelText = document.querySelector(".text");
+const dark = document.querySelectorAll(".dark");
+
+// Light mode
+const lightMode = function () {
+  for (let i = 0; i < dark.length; i++) {
+    dark[i].classList.remove("dark");
+  }
+};
+
+// Dark mode
+const darkMode = function () {
+  for (let i = 0; i < dark.length; i++) {
+    dark[i].classList.add("dark");
+  }
+};
+
+document.onclick = function (event) {
+  console.log(event);
+  if (event.target.checked === true) {
+    lightMode();
+    labelText.innerText = "Light Mode";
+    toggleBtn.style.background = "hsl(230, 22%, 74%)";
+  } else {
+    darkMode();
+    labelText.innerText = "Dark Mode";
+  }
+};
